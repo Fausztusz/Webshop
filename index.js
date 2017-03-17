c7f76d9402a2af0e7990e47e5ea53d59568dec1b
Fausztusz <Ocioci166#fauszt.andras19@gmail.com>
2017-03-17 20:57:48 +0100

    Whatever

--- index.js
+++ index.js
@@ -4,17 +4,20 @@ var app= express();
 
 app.use(express.static('static'));
 
-require('./routes/employeelist')(app);
-require('./routes/inventorylist')(app);
-require('./routes/itemlist')(app);
-require('./routes/outside')(app);
-
-
-
-app.use(function (req,res,next) {
+//require('./routes/productlist')(app);
+//require('./routes/userlist')(app);
+//require('./routes/itemlist')(app);
+//require('./routes/outside')(app);
+//require('./routes/productlist')(app);
+/*app.use(function (req,res,next) {
    res.tpl={};
    return next();
-});
+});*/
+app.get('/pay',function(req, res, next){
+        res.redirect('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+    }
+);
+
 
 var server=app.listen(928, function () {
     console.log('Listening to port 928')
