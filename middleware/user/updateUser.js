f36d53392762c88d32663a5d41bd5fbde1ad694b
jonkom7 <gergely.daniel7@gmail.com>
2017-05-18 03:48:38 +0200

    Test

--- middleware/user/updateUser.js
+++ middleware/user/updateUser.js
@@ -6,31 +6,26 @@ var requireOption = require('../common').requireOption;
 
 
 module.exports = function (objectRepository) {
-    var User = requireOption(objectRepository, 'userModel');
-    return function (req, res, next) {
+	var User = requireOption(objectRepository, 'userModel');
+	return function (req, res, next) {
 
-        var options = {};
-        options = req.body;
-        var userID = req.body._id;
-        // If the user left the field blank the value don't change
-        delete options._id; //Protects the original _id
-        if (!options.name) delete options.name;
-        if (!options.email) delete options.email;
-        if (!options.picture) delete options.picture;
-        if (!(options.role === 0 || options.role === 1)) delete options.role; //Prevents invalid role commit
+		var options = req.body;
+		var userID = req.body._id;
+		// If the user left the field blank the value don't change
+		delete options._id; //Protects the original _id
+		if (!options.name) delete options.name;
+		if (!options.email) delete options.email;
+		if (!options.picture) delete options.picture;
+		if (!(options.role == 0 || options.role == 1)) delete options.role; //Prevents invalid role commit
 
-
-        User.findByIdAndUpdate(userID, {$set: options}, function (err, cb) {
-            if (err) {
-                console.log('##### Failed query at updateUser ######');
-                console.log(err);
-                res.error = err;
-                next();
-            }
-            else {
-                return next();
-            }
-        });
-    };
+		User.findByIdAndUpdate(userID, {$set: options}, function (err) {
+			if (err) {
+				//console.log('##### Failed query at updateUser ######');
+				//console.log(err);
+				res.error = err;
+			}
+			return next();
+		});
+	};
 
 };
